223f93d56937cd038b791fb94dff4655553607dc
Joao Rocha <joaogsrocha@gmail.com>
2015-11-22 23:40:17 +0000

    Centered youtube video on widget container Though, it will need be adapted, so the youtube video player and the table surrounding resizes according the resized widget container. As well adding the features to resize the widget container(which the authors of gridster already created). After this, it is necessary to add an icon that fades in when the user hover the mouse point over one of the side cells of the table. There will be an icon for each cell, so the user feels comfortable in dragging wherever side it prefers to choose.

--- jquery.gridder_test02.js
+++ jquery.gridder_test02.js
@@ -1,14 +1,23 @@
 
-  $(function(){ //DOM Ready
+ $(function(){ //DOM Ready
 
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     });
-	var myElement = '<li class="new"><div class="vid"><iframe autoplay="1" width="232" height="100" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></div> </li>';
+	var myElement ='<li><table border ="0" class="vid"><tr><td>fdf</td><td></td><td></td></tr><tr><td>13</td><td><iframe autoplay="1" width="390" height="235" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></td><td></td></tr><tr><td>fdf</td><td></td><td>fdd</td></tr></table></li>';
 	myElement.display = "inline-block";
 	myElement.align = "middle";
 	myElement.lineHeight = "normal";
 	var gridster = $(".gridster ul").gridster().data('gridster');
-	gridster.add_widget(myElement , 2, 1);
+	gridster.add_widget(myElement , 3, 2);
 });
+/*
+function bigImg(x) {
+	x.style.height = "64px";
+	x.style.width = "64px";
+}
+function normalImg(x) {
+	x.style.height = "32px";
+	x.style.width = "32px";
+}*/
