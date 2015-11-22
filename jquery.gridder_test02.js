a375822b8b12a097632484584e8d9aa13ffe64d6
Joao Rocha <joaogsrocha@gmail.com>
2015-11-22 19:27:37 +0000

    Added youtube video to the widgets, Though, it will need be adapted, so the youtube pause do not conflict with the interaction of dragging and moving the widgets

--- jquery.gridder_test02.js
+++ jquery.gridder_test02.js
@@ -5,6 +5,10 @@
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     });
+	var myElement = '<li class="new"><div class="vid"><iframe autoplay="1" width="232" height="100" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></div> </li>';
+	myElement.display = "inline-block";
+	myElement.align = "middle";
+	myElement.lineHeight = "normal";
 	var gridster = $(".gridster ul").gridster().data('gridster');
-	gridster.add_widget('<li class="new">The HTML of the widget...</li>', 2, 1);
+	gridster.add_widget(myElement , 2, 1);
 });
