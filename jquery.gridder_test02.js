7345e98827cde91403fefb65715fd2a23c7354c3
Joao Rocha <joaogsrocha@gmail.com>
2015-11-24 00:09:14 +0000

    Removed pink layout border and added drag icon

--- jquery.gridder_test02.js
+++ jquery.gridder_test02.js
@@ -5,19 +5,17 @@
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     });
-	var myElement ='<li><table border ="0" class="vid"><tr><td>fdf</td><td></td><td></td></tr><tr><td>13</td><td><iframe autoplay="1" width="390" height="235" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></td><td></td></tr><tr><td>fdf</td><td></td><td>fdd</td></tr></table></li>';
+	var myElement ='<li><table border ="0" class="vid"><tr><td width="40px"></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;</div></td><td></td></tr><tr><td onmouseover="bigImg(this)" onmouseout="normalImg(this)"><img src="dragIcon.png"></td><td><iframe autoplay="1" width="390" height="235" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></td></tr><tr><td></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;</div></td><td></td></tr></table></li>';
 	myElement.display = "inline-block";
 	myElement.align = "middle";
 	myElement.lineHeight = "normal";
 	var gridster = $(".gridster ul").gridster().data('gridster');
 	gridster.add_widget(myElement , 3, 2);
 });
-/*
+
 function bigImg(x) {
-	x.style.height = "64px";
-	x.style.width = "64px";
+	x.style.style.opacity = "0.3";
 }
 function normalImg(x) {
-	x.style.height = "32px";
-	x.style.width = "32px";
-}*/
+	x.style.style.opacity = "0.0";
+}
