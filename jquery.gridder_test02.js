1eeba9ac34054e2455d271fdb11510570f8a86b2
Joao Rocha <joaogsrocha@gmail.com>
2015-11-24 02:42:07 +0000

    Changed oppacity of Drag icon when out/in of video frame

--- jquery.gridder_test02.js
+++ jquery.gridder_test02.js
@@ -5,7 +5,7 @@
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     });
-	var myElement ='<li><table border ="0" class="vid"><tr><td width="40px"></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;</div></td><td></td></tr><tr><td onmouseover="bigImg(this)" onmouseout="normalImg(this)"><img src="dragIcon.png"></td><td><iframe autoplay="1" width="390" height="235" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></td></tr><tr><td></td><td><div class="dragIcon" onmouseover="bigImg(this)" onmouseout="normalImg(this)">&nbsp;</div></td><td></td></tr></table></li>';
+	var myElement ='<li><table border ="0" class="vid"><tr><td width="40px" class="dragIcon"></td><td class="dragIcon"><img class="dragIcon" src="dragIcon.png">&nbsp;</td><td class="dragIcon"></td></tr><tr><td class="dragIcon">&nbsp;&nbsp;&nbsp;</td><td><iframe autoplay="1" width="390" height="235" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=0"></iframe></td><td class="dragIcon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td class="dragIcon"></td><td class="dragIcon">&nbsp;</td><td class="dragIcon"></td></tr></table></li>';
 	myElement.display = "inline-block";
 	myElement.align = "middle";
 	myElement.lineHeight = "normal";
@@ -13,9 +13,12 @@
 	gridster.add_widget(myElement , 3, 2);
 });
 
-function bigImg(x) {
-	x.style.style.opacity = "0.3";
-}
-function normalImg(x) {
-	x.style.style.opacity = "0.0";
-}
+	$(document).ready(function(){
+		$(".dragIcon").hover(
+			function(){
+			$("img.dragIcon").css('opacity', '1.0');
+			}, function(){
+			$("img.dragIcon").css('opacity', '0.0');
+			});
+		
+	});
